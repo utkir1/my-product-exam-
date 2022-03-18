5c80184163d4183a0c34b2ec383a38c69ee31373
utkir1 <otkirsoatboyev1@gmail.com>
2022-03-18 21:13:43 +0500

    index.js added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-const burger = document.querySelector(".header__burger");
-const nav = document.querySelector(".header__nav");
-const body = document.querySelector("body");
 
-burger.addEventListener("click", function() {
-  burger.classList.toggle("header__burger--opened");
-  nav.classList.toggle("header__nav--opened");
-  body.classList.toggle("freez")
-})
 
 let plusBtn = document.querySelector(".questions__item-btn")
 let questionText = document.querySelector(".questions__item-desc")
@@ -20,4 +12,3 @@ plusBtn.addEventListener("click", function(){
   }
 
 })
-
