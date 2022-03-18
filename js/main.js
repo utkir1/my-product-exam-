ed93b48557dffda77c5e0a1097c055dc6376f627
QoriDev <bbekturayev030602@gmail.com>
2022-03-18 19:41:26 +0200

    added aids section and js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+const burger = document.querySelector(".header__burger");
+const nav = document.querySelector(".header__nav");
+const body = document.querySelector("body");
+
+burger.addEventListener("click", function() {
+  burger.classList.toggle("header__burger--opened");
+  nav.classList.toggle("header__nav--opened");
+  body.classList.toggle("freez")
+})
+
 let plusBtn = document.querySelector(".questions__item-btn")
 let questionText = document.querySelector(".questions__item-desc")
 
@@ -10,3 +20,4 @@ plusBtn.addEventListener("click", function(){
   }
 
 })
+
